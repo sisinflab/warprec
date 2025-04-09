f1e1a76881f8b577a253b4bc43f317a2d6c12b43
MarcoWV <marco@wideverse.com>
2025-04-09 14:29:25 +0200

    Model param validation separate directory

--- elliotwo/utils/config/__init__.py
+++ elliotwo/utils/config/__init__.py
@@ -8,6 +8,7 @@ from .reader_configuration import (
 from .writer_configuration import WriterConfig, WritingParams
 from .splitter_configuration import SplittingConfig
 from .model_configuration import RecomModel
+from .recommender_model_config import EASE, ItemKNN, UserKNN, Slim, NeuMF
 from .evaluation_configuration import EvaluationConfig
 from .search_space_wrapper import SearchSpaceWrapper
 from .config import Configuration, load_yaml
@@ -24,6 +25,11 @@ __all__ = [
     "WritingParams",
     "SplittingConfig",
     "RecomModel",
+    "EASE",
+    "ItemKNN",
+    "UserKNN",
+    "Slim",
+    "NeuMF",
     "EvaluationConfig",
     "SearchSpaceWrapper",
 ]
---
+++ elliotwo/utils/config/recommender_model_config/__init__.py
@@ -0,0 +1,3 @@
+from .general_recommender_config import EASE, ItemKNN, UserKNN, Slim, NeuMF
+
+__all__ = ["EASE", "ItemKNN", "UserKNN", "Slim", "NeuMF"]
