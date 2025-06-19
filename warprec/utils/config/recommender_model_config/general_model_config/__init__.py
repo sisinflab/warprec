5c0701bea46f3f022cf771200f414b2288b7909d
MarcoWV <marco@wideverse.com>
2025-06-19 11:12:01 +0200

    Knn hyperparam validation

--- warprec/utils/config/recommender_model_config/general_model_config/__init__.py
+++ warprec/utils/config/recommender_model_config/general_model_config/__init__.py
@@ -1,5 +1,19 @@
 from .autoencoder_config import AddEASE, CEASE, EASE, MultiDAE
 from .content_based_config import VSM
 from .graph_based_config import LightGCN, NGCF, RP3Beta
+from .knn_config import AttributeItemKNN, AttributeUserKNN, ItemKNN, UserKNN
 
-__all__ = ["AddEASE", "CEASE", "EASE", "MultiDAE", "VSM", "LightGCN", "NGCF", "RP3Beta"]
+__all__ = [
+    "AddEASE",
+    "CEASE",
+    "EASE",
+    "MultiDAE",
+    "VSM",
+    "LightGCN",
+    "NGCF",
+    "RP3Beta",
+    "AttributeItemKNN",
+    "AttributeUserKNN",
+    "ItemKNN",
+    "UserKNN",
+]
