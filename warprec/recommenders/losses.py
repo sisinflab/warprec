Bugfix: Removed padding embedding from predictions and correction to number of negative samples inside loss computation

warprec/recommenders/losses.py
@@ -26,7 +26,7 @@ class BPRLoss(nn.Module):
         """
         # Compute the distance of positive
         # and negative scores
-        distance = pos_score - neg_score
+        distance = pos_score.unsqueeze(1) - neg_score
 
         # Compute the softplus function of the negative distance
         loss = F.softplus(-distance)  # pylint: disable=not-callable

warprec/recommenders/sequential_recommender/caser.py
@@ -32,7 +32,7 @@ class Caser(Recommender):
         **kwargs (Any): Arbitrary keyword arguments.
 
     Raises:
-        ValueError: If essential values like 'items', 'users' or 'max_seq_len'
+        ValueError: If essential values like 'items' or 'users'
                     are not passed through the info dict.
 
     Attributes:
@@ -251,7 +251,9 @@ class Caser(Recommender):
                     neg_items_emb = self.item_embedding(neg_item)
 
                     pos_score = torch.sum(seq_output * pos_items_emb, dim=-1)
-                    neg_score = torch.sum(seq_output * neg_items_emb, dim=-1)
+                    neg_score = torch.sum(
+                        seq_output.unsqueeze(1) * neg_items_emb, dim=-1
+                    )
                     total_loss = self.loss(pos_score, neg_score)
                 else:  # Cross-Entropy Loss
                     test_item_emb = self.item_embedding.weight
@@ -295,7 +297,7 @@ class Caser(Recommender):
         seq_output = self.forward(user_ids, user_seq)
 
         # Get embeddings for all items
-        all_item_embeddings = self.item_embedding.weight
+        all_item_embeddings = self.item_embedding.weight[1:]
 
         # Calculate scores for all items via dot product
         predictions = torch.matmul(seq_output, all_item_embeddings.transpose(0, 1))

warprec/recommenders/sequential_recommender/fossil.py
@@ -84,7 +84,7 @@ class FOSSIL(Recommender, SequentialRecommenderUtils):
             self.n_items + 1, self.embedding_size, padding_idx=0
         )
         self.user_lambda = nn.Embedding(
-            self.n_users + 1, self.order_len
+            self.n_users, self.order_len
         )  # User specific weights for Markov chains
         self.lambda_ = nn.Parameter(
             torch.zeros(self.order_len)
@@ -337,7 +337,9 @@ class FOSSIL(Recommender, SequentialRecommenderUtils):
                 if self.neg_samples > 0:
                     neg_items_emb = self.item_embedding(neg_item)
                     pos_score = torch.sum(seq_output * pos_items_emb, dim=-1)
-                    neg_score = torch.sum(seq_output * neg_items_emb, dim=-1)
+                    neg_score = torch.sum(
+                        seq_output.unsqueeze(1) * neg_items_emb, dim=-1
+                    )
                     total_loss = self.loss(pos_score, neg_score)
                     total_loss += self._reg_loss(
                         user_lambda_emb, pos_items_emb, seq_output
@@ -364,7 +366,7 @@ class FOSSIL(Recommender, SequentialRecommenderUtils):
         interaction_matrix: csr_matrix,
         user_seq: Tensor,
         seq_len: Tensor,
-        user_id_batch: Tensor,
+        user_id: Tensor,
         *args: Any,
         **kwargs: Any,
     ) -> Tensor:
@@ -376,7 +378,7 @@ class FOSSIL(Recommender, SequentialRecommenderUtils):
                 pairs of interactions to evaluate.
             user_seq (Tensor): Padded sequences of item IDs for users to predict for.
             seq_len (Tensor): Actual lengths of these sequences, before padding.
-            user_id_batch (Tensor): The user IDs corresponding to user_seq.
+            user_id (Tensor): The user IDs corresponding to user_seq.
             *args (Any): List of arguments.
             **kwargs (Any): The dictionary of keyword arguments.
 
@@ -385,15 +387,18 @@ class FOSSIL(Recommender, SequentialRecommenderUtils):
         """
         user_seq = user_seq.to(self._device)
         seq_len = seq_len.to(self._device)
-        user_id_batch = user_id_batch.to(self._device)
+        user_id = user_id.to(self._device)
+        print(user_id.min(), user_id.max())
 
         # Get the combined output embedding for each user
         seq_output = self.forward(
-            user_seq, seq_len, user_id_batch
+            user_seq, seq_len, user_id
         )  # [num_users, embedding_size]
 
         # Get embeddings for all items
-        all_item_embeddings = self.item_embedding.weight  # [n_items, embedding_size]
+        all_item_embeddings = self.item_embedding.weight[
+            1:
+        ]  # [n_items, embedding_size]
 
         # Calculate scores for all items
         # Scores = dot product of session embedding with all item embeddings

warprec/recommenders/sequential_recommender/gru4rec.py
@@ -224,7 +224,7 @@ class GRU4Rec(Recommender, SequentialRecommenderUtils):
                         seq_output * pos_items_emb, dim=-1
                     )  # [batch_size]
                     neg_score = torch.sum(
-                        seq_output * neg_items_emb, dim=-1
+                        seq_output.unsqueeze(1) * neg_items_emb, dim=-1
                     )  # [batch_size]
                     total_loss = self.loss(pos_score, neg_score)
                 else:
@@ -277,7 +277,9 @@ class GRU4Rec(Recommender, SequentialRecommenderUtils):
         seq_output = self.forward(user_seq, seq_len)  # [num_users, embedding_size]
 
         # Get embeddings for all items
-        all_item_embeddings = self.item_embedding.weight  # [n_items, embedding_size]
+        all_item_embeddings = self.item_embedding.weight[
+            1:
+        ]  # [n_items, embedding_size]
 
         # Calculate scores for all items
         # Scores = dot product of session embedding with all item embeddings

warprec/recommenders/sequential_recommender/sasrec.py
@@ -219,7 +219,7 @@ class SASRec(Recommender, SequentialRecommenderUtils):
         self.train()
         for _ in range(self.epochs):
             epoch_loss = 0.0
-            for batch in dataloader:
+            for i, batch in enumerate(dataloader):
                 if self.neg_samples > 0:
                     item_seq, item_seq_len, pos_item, neg_item = [
                         x.to(self._device) for x in batch
@@ -250,7 +250,7 @@ class SASRec(Recommender, SequentialRecommenderUtils):
                         seq_output * pos_items_emb, dim=-1
                     )  # [batch_size]
                     neg_score = torch.sum(
-                        seq_output * neg_items_emb, dim=-1
+                        seq_output.unsqueeze(1) * neg_items_emb, dim=-1
                     )  # [batch_size]
                     total_loss = self.loss(pos_score, neg_score)
                 else:
@@ -303,7 +303,9 @@ class SASRec(Recommender, SequentialRecommenderUtils):
         seq_output = self.forward(user_seq, seq_len)  # [num_users, embedding_size]
 
         # Get embeddings for all items
-        all_item_embeddings = self.item_embedding.weight  # [n_items, embedding_size]
+        all_item_embeddings = self.item_embedding.weight[
+            1:
+        ]  # [n_items, embedding_size]
 
         # Calculate scores for all items
         # Scores = dot product of session embedding with all item embeddings