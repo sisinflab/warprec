0b83e97e2a999472514bb3547cb271a76dfeef22
MarcoWV <marco@wideverse.com>
2025-07-24 14:45:03 +0200

    MAE metric user_wise implementation

--- warprec/evaluation/metrics/rating/mae.py
+++ warprec/evaluation/metrics/rating/mae.py
@@ -39,42 +39,77 @@ class MAE(BaseMetric):
     Attributes:
         sum_absolute_errors (Tensor): Sum of absolute errors across all batches.
         total_count (Tensor): Total number of elements processed.
+        compute_per_user (bool): Wether or not to compute the metric
+            per user or globally.
 
     Args:
+        num_users (int): Number of users in the training set.
         *args (Any): The argument list.
+        compute_per_user (bool): Wether or not to compute the metric
+            per user or globally.
         dist_sync_on_step (bool): Torchmetrics parameter.
         **kwargs (Any): The keyword argument dictionary.
     """
 
     sum_absolute_errors: Tensor
     total_count: Tensor
+    compute_per_user: bool
 
-    def __init__(self, *args: Any, dist_sync_on_step: bool = False, **kwargs: Any):
-        super().__init__(dist_sync_on_step=dist_sync_on_step)
-        self.add_state(
-            "sum_absolute_errors", default=torch.tensor(0.0), dist_reduce_fx="sum"
-        )
-        self.add_state("total_count", default=torch.tensor(0.0), dist_reduce_fx="sum")
+    def __init__(
+        self,
+        num_users: int,
+        *args: Any,
+        compute_per_user: bool = False,
+        dist_sync_on_step: bool = False,
+        **kwargs: Any,
+    ):
+        super().__init__()
+        self.compute_per_user = compute_per_user
 
-    def update(self, preds: Tensor, **kwargs: Any):
+        if self.compute_per_user:
+            # Initialize a tensor to store metric value for each user
+            self.add_state(
+                "sum_absolute_errors",
+                default=torch.zeros(num_users),
+                dist_reduce_fx="sum",
+            )
+            self.add_state(
+                "total_count", default=torch.zeros(num_users), dist_reduce_fx="sum"
+            )
+        else:
+            # Initialize a scalar to store global value
+            self.add_state(
+                "sum_absolute_errors", default=torch.tensor(0.0), dist_reduce_fx="sum"
+            )
+            self.add_state(
+                "total_count", default=torch.tensor(0.0), dist_reduce_fx="sum"
+            )
+
+    def update(self, preds: Tensor, user_indices: Tensor, **kwargs: Any):
         """Updates the metric state with the new batch of predictions."""
         target = kwargs.get("ground", torch.zeros_like(preds))
 
         mask = target > 0
-        abs_errors = torch.abs(preds[mask] - target[mask])
-        self.sum_absolute_errors += abs_errors.sum()
-        self.total_count += torch.count_nonzero(target)
+        abs_errors = torch.zeros_like(preds)
+        abs_errors[mask] = torch.abs(preds[mask] - target[mask])
+
+        if self.compute_per_user:
+            self.sum_absolute_errors.index_add_(0, user_indices, abs_errors.sum(dim=1))
+            self.total_count.index_add_(
+                0, user_indices, torch.count_nonzero(target, dim=1).float()
+            )
+        else:
+            self.sum_absolute_errors += abs_errors.sum()
+            self.total_count += torch.count_nonzero(target)
 
     def compute(self):
         """Computes the final metric value."""
-        mae = (
-            self.sum_absolute_errors / self.total_count
-            if self.total_count > 0
-            else torch.tensor(0.0)
-        )
-        return {self.name: mae.item()}
-
-    def reset(self):
-        """Reset the metric state."""
-        self.sum_absolute_errors.zero_()
-        self.total_count.zero_()
+        if self.compute_per_user:
+            mae = (self.sum_absolute_errors / self.total_count).nan_to_num(0)
+        else:
+            mae = (
+                self.sum_absolute_errors / self.total_count
+                if self.total_count > 0
+                else torch.tensor(0.0)
+            ).item()
+        return {self.name: mae}
