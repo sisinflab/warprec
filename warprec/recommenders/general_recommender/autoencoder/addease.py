501f319676eea0fd088ac82f642252e7fca010f9
MarcoWV <marco@wideverse.com>
2025-08-05 12:25:28 +0200

    AddEASE new interface

--- warprec/recommenders/general_recommender/autoencoder/addease.py
+++ warprec/recommenders/general_recommender/autoencoder/addease.py
@@ -1,5 +1,5 @@
 # pylint: disable = R0801, E1102
-from typing import Optional, Callable, Any
+from typing import Any
 
 import numpy as np
 import torch
@@ -18,6 +18,7 @@ class AddEASE(ItemSimRecommender):
 
     Args:
         params (dict): The dictionary with the model params.
+        interactions (Interactions): The training interactions.
         *args (Any): Argument for PyTorch nn.Module.
         device (str): The device used for tensor operations.
         seed (int): The seed to use for reproducibility.
@@ -35,35 +36,18 @@ class AddEASE(ItemSimRecommender):
     def __init__(
         self,
         params: dict,
+        interactions: Interactions,
         *args: Any,
         device: str = "cpu",
         seed: int = 42,
         info: dict = None,
         **kwargs: Any,
     ):
-        super().__init__(params, device=device, seed=seed, info=info, *args, **kwargs)
+        super().__init__(
+            params, interactions, device=device, seed=seed, info=info, *args, **kwargs
+        )
         self._name = "AddEASE"
 
-    def fit(
-        self,
-        interactions: Interactions,
-        *args: Any,
-        report_fn: Optional[Callable] = None,
-        **kwargs: Any,
-    ):
-        """Main train method.
-
-        The training will be conducted on the sparse representation of the interactions
-        and the sparse representation of the features.
-        During the train a similarity matrix {item x item} will be learned.
-
-        Args:
-            interactions (Interactions): The interactions that will be used to train the model.
-            *args (Any): List of arguments.
-            report_fn (Optional[Callable]): The Ray Tune function to report the iteration.
-            **kwargs (Any): The dictionary of keyword arguments.
-        """
-        # The classic implementation follows the original paper
         X = interactions.get_sparse()
         item_profile = interactions.get_side_sparse()
 
@@ -83,6 +67,3 @@ class AddEASE(ItemSimRecommender):
         sim = self.alpha * B + (1 - self.alpha) * U
 
         self.item_similarity = nn.Parameter(torch.tensor(sim, dtype=torch.float32))
-
-        if report_fn is not None:
-            report_fn(self)
