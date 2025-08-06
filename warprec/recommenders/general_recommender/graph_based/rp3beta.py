35fd0165838e433bfcdeb3792ff9e27578fbe833
MarcoWV <marco@wideverse.com>
2025-08-06 11:43:31 +0200

    RP3Beta new interface

--- warprec/recommenders/general_recommender/graph_based/rp3beta.py
+++ warprec/recommenders/general_recommender/graph_based/rp3beta.py
@@ -1,5 +1,5 @@
 # pylint: disable = R0801, E1102
-from typing import Union, Optional, Callable, Any
+from typing import Union, Any
 
 import torch
 import numpy as np
@@ -20,6 +20,7 @@ class RP3Beta(ItemSimRecommender):
 
     Args:
         params (dict): Model parameters.
+        interactions (Interactions): The training interactions.
         *args (Any): Variable length argument list.
         device (str): The device used for tensor operations.
         seed (int): The seed to use for reproducibility.
@@ -41,33 +42,18 @@ class RP3Beta(ItemSimRecommender):
     def __init__(
         self,
         params: dict,
+        interactions: Interactions,
         *args: Any,
         device: str = "cpu",
         seed: int = 42,
         info: dict = None,
         **kwargs: Any,
     ):
-        super().__init__(params, device=device, seed=seed, info=info, *args, **kwargs)
+        super().__init__(
+            params, interactions, device=device, seed=seed, info=info, *args, **kwargs
+        )
         self._name = "RP3Beta"
 
-    def fit(
-        self,
-        interactions: Interactions,
-        *args: Any,
-        report_fn: Optional[Callable] = None,
-        **kwargs: Any,
-    ):
-        """Main train method.
-
-        The training will be conducted on the sparse representation of the interactions.
-        During the train a similarity matrix {item x item} will be learned.
-
-        Args:
-            interactions (Interactions): The interactions that will be used to train the model.
-            *args (Any): List of arguments.
-            report_fn (Optional[Callable]): The Ray Tune function to report the iteration.
-            **kwargs (Any): The dictionary of keyword arguments.
-        """
         X = interactions.get_sparse()
 
         # Step 1: Normalize user-item matrix
@@ -103,9 +89,6 @@ class RP3Beta(ItemSimRecommender):
 
         # Update item_similarity with a new nn.Parameter, doing the proper casting
         self.item_similarity = nn.Parameter(torch.from_numpy(filtered_matrix.toarray()))
-
-        if report_fn is not None:
-            report_fn(self)
 
     def _compute_blockwise_similarity(
         self,
