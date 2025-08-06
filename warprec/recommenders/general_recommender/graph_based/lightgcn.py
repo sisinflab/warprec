6aeba9d7f4252fb1896726ef847a4aceb45593cc
MarcoWV <marco@wideverse.com>
2025-08-06 11:15:31 +0200

    LightGCN new interface + removed block_size

--- warprec/recommenders/general_recommender/graph_based/lightgcn.py
+++ warprec/recommenders/general_recommender/graph_based/lightgcn.py
@@ -1,5 +1,5 @@
 # pylint: disable = R0801, E1102
-from typing import Optional, Callable, Tuple, Any
+from typing import Tuple, Any
 
 import torch
 import torch_geometric
@@ -7,17 +7,18 @@ from torch import nn, Tensor
 from torch.nn import Module
 from torch.nn.init import xavier_normal_
 from torch_geometric.nn import LGConv
-from torch_sparse import SparseTensor
-from scipy.sparse import csr_matrix
 
 from warprec.data.dataset import Interactions
-from warprec.recommenders.base_recommender import Recommender, GraphRecommenderUtils
+from warprec.recommenders.base_recommender import (
+    IterativeRecommender,
+    GraphRecommenderUtils,
+)
 from warprec.recommenders.losses import BPRLoss
 from warprec.utils.registry import model_registry
 
 
 @model_registry.register(name="LightGCN")
-class LightGCN(Recommender, GraphRecommenderUtils):
+class LightGCN(IterativeRecommender, GraphRecommenderUtils):
     """Implementation of LightGCN algorithm from
         LightGCN: Simplifying and Powering Graph Convolution Network for Recommendation (SIGIR 2020)
 
@@ -25,6 +26,7 @@ class LightGCN(Recommender, GraphRecommenderUtils):
 
     Args:
         params (dict): Model parameters.
+        interactions (Interactions): The training interactions.
         *args (Any): Variable length argument list.
         device (str): The device used for tensor operations.
         seed (int): The seed to use for reproducibility.
@@ -54,13 +56,16 @@ class LightGCN(Recommender, GraphRecommenderUtils):
     def __init__(
         self,
         params: dict,
+        interactions: Interactions,
         *args: Any,
         device: str = "cpu",
         seed: int = 42,
         info: dict = None,
         **kwargs: Any,
     ):
-        super().__init__(params, device=device, seed=seed, *args, **kwargs)
+        super().__init__(
+            params, interactions, device=device, seed=seed, *args, **kwargs
+        )
         self._name = "LightGCN"
 
         # Get information from dataset info
@@ -74,19 +79,15 @@ class LightGCN(Recommender, GraphRecommenderUtils):
             raise ValueError(
                 "Items value must be provided to correctly initialize the model."
             )
-        self.block_size = kwargs.get("block_size", 50)
 
         self.user_embedding = nn.Embedding(self.n_users, self.embedding_size)
         self.item_embedding = nn.Embedding(self.n_items, self.embedding_size)
-
-        # Init embedding weights
-        self.apply(self._init_weights)
-
-        self.optimizer = torch.optim.Adam(
-            self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay
+        self.adj = self.get_adj_mat(
+            interactions.get_sparse().tocoo(),
+            self.n_users,
+            self.n_items,
+            self._device,
         )
-        self.loss = BPRLoss()
-        self.adj: Optional[SparseTensor] = None
 
         # Initialization of the propagation network
         propagation_network_list = []
@@ -101,6 +102,11 @@ class LightGCN(Recommender, GraphRecommenderUtils):
             [1 / (k + 1) for k in range(self.n_layers + 1)], device=self._device
         )
 
+        # Init embedding weights
+        self.apply(self._init_weights)
+        self.loss = BPRLoss()
+
+        # Move to device
         self.to(self._device)
 
     def _init_weights(self, module: Module):
@@ -112,61 +118,31 @@ class LightGCN(Recommender, GraphRecommenderUtils):
         if isinstance(module, nn.Embedding):
             xavier_normal_(module.weight.data)
 
-    def fit(
-        self,
-        interactions: Interactions,
-        *args: Any,
-        report_fn: Optional[Callable] = None,
-        **kwargs: Any,
-    ):
-        """Main train method for LightGCN.
+    def get_optimizer(self):
+        return torch.optim.Adam(
+            self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay
+        )
 
-        Args:
-            interactions (Interactions): The interactions that will be used to train the model.
-            *args (Any): List of arguments.
-            report_fn (Optional[Callable]): The Ray Tune function to report the iteration.
-            **kwargs (Any): The dictionary of keyword arguments.
-        """
-        if self.adj is None:
-            self.adj = self._get_adj_mat(
-                interactions.get_sparse().tocoo(),
-                self.n_users,
-                self.n_items,
-                self._device,
-            )
+    def get_dataloader(self, interactions: Interactions, **kwargs):
+        return interactions.get_pos_neg_dataloader(self.batch_size)
 
-        # Get the dataloader from interactions for pairwise training
-        dataloader = interactions.get_pos_neg_dataloader(self.batch_size)
+    def train_step(self, batch: Any, *args, **kwargs):
+        user, pos_item, neg_item = [x.to(self._device) for x in batch]
 
-        self.train()
-        for _ in range(self.epochs):
-            epoch_loss = 0.0
-            for batch in dataloader:
-                user, pos_item, neg_item = [x.to(self._device) for x in batch]
+        # Get propagated embeddings
+        user_all_embeddings, item_all_embeddings = self.forward()
 
-                self.optimizer.zero_grad()
+        # Get embeddings for current batch users and items
+        u_embeddings = user_all_embeddings[user]
+        pos_embeddings = item_all_embeddings[pos_item]
+        neg_embeddings = item_all_embeddings[neg_item]
 
-                # Get propagated embeddings
-                user_all_embeddings, item_all_embeddings = self.forward()
+        # Calculate BPR Loss
+        pos_scores = torch.mul(u_embeddings, pos_embeddings).sum(dim=1)
+        neg_scores = torch.mul(u_embeddings, neg_embeddings).sum(dim=1)
+        loss: Tensor = self.loss(pos_scores, neg_scores)
 
-                # Get embeddings for current batch users and items
-                u_embeddings = user_all_embeddings[user]
-                pos_embeddings = item_all_embeddings[pos_item]
-                neg_embeddings = item_all_embeddings[neg_item]
-
-                # Calculate BPR Loss
-                pos_scores = torch.mul(u_embeddings, pos_embeddings).sum(dim=1)
-                neg_scores = torch.mul(u_embeddings, neg_embeddings).sum(dim=1)
-                loss: Tensor = self.loss(pos_scores, neg_scores)
-
-                # Loss computation and backpropagation
-                loss.backward()
-                self.optimizer.step()
-
-                epoch_loss += loss.item()
-
-            if report_fn is not None:
-                report_fn(self, loss=epoch_loss)
+        return loss
 
     def forward(self) -> Tuple[Tensor, Tensor]:
         """Forward pass of the LightGCN model for embedding propagation.
@@ -174,7 +150,7 @@ class LightGCN(Recommender, GraphRecommenderUtils):
         Returns:
             Tuple[Tensor, Tensor]: User and item embeddings after propagation.
         """
-        ego_embeddings = self._get_ego_embeddings(
+        ego_embeddings = self.get_ego_embeddings(
             self.user_embedding, self.item_embedding
         )
         embeddings_list = [ego_embeddings]
@@ -200,47 +176,36 @@ class LightGCN(Recommender, GraphRecommenderUtils):
 
     @torch.no_grad()
     def predict(
-        self, interaction_matrix: csr_matrix, *args: Any, **kwargs: Any
+        self,
+        train_batch: Tensor,
+        user_indices: Tensor,
+        *args: Any,
+        **kwargs: Any,
     ) -> Tensor:
-        """Prediction using the learned embeddings."""
-        if self.adj is None:
-            train_set: Interactions = kwargs.get("train_set", None)
-            if train_set is None:
-                raise RuntimeError(
-                    "The model has not been fit yet. Call fit() before predict() "
-                    "or pass the training set as kwargs."
-                )
-            self.adj = self._get_adj_mat(
-                train_set.get_sparse().tocoo(), self.n_users, self.n_items, self._device
-            )
+        """Prediction using the learned embeddings.
 
-        # Perform forward pass to get the final embeddings
-        user_e, item_e = self.forward()
+        Args:
+            train_batch (Tensor): The train batch of user interactions.
+            user_indices (Tensor): The batch of user indices.
+            *args (Any): List of arguments.
+            **kwargs (Any): The dictionary of keyword arguments.
 
-        start_idx = kwargs.get("start", 0)
-        end_idx = kwargs.get("end", interaction_matrix.shape[0])
+        Returns:
+            Tensor: The score matrix {user x item}.
+        """
+        user_e, item_e = (
+            self.forward()
+        )  # [n_users, embedding_size], [n_items, embedding_size]
 
-        preds = []
-        # Process users in batches to manage memory
-        for current_batch_start in range(start_idx, end_idx, self.block_size):
-            current_batch_end = min(current_batch_start + self.block_size, end_idx)
-            users_in_batch = torch.arange(
-                current_batch_start, current_batch_end, device=self._device
-            )
+        # Get the embeddings for the specific users in the batch
+        u_embeddings_batch = user_e[user_indices]  # [batch_size, embedding_size]
 
-            # Get user embeddings for the current batch
-            u_embeddings = user_e[users_in_batch]
+        # 3. Compute all item scores for the current user batch
+        # This is a dot product between the user embedding batch and all item embeddings.
+        predictions = torch.matmul(
+            u_embeddings_batch, item_e.transpose(0, 1)
+        )  # [batch_size, n_items]
 
-            # Calculate scores by dot product with all item embeddings
-            # This is the "full_sort_predict" logic
-            scores_batch = torch.matmul(u_embeddings, item_e.transpose(0, 1))
-            preds.append(scores_batch)
-
-        predictions = torch.cat(preds, dim=0)  # [batch_size x num_items]
-
-        coo = interaction_matrix.tocoo()
-        user_indices_in_batch = torch.from_numpy(coo.row).to(self._device)
-        item_indices = torch.from_numpy(coo.col).to(self._device)
-        predictions[user_indices_in_batch, item_indices] = -torch.inf
-
-        return predictions
+        # Masking interaction already seen in train
+        predictions[train_batch != 0] = -torch.inf
+        return predictions.to(self._device)
