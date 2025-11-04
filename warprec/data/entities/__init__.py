ab9e4bfa9e134456debe5e17d23d89caccb7a4be
MarcoWV <marco@wideverse.com>
2025-11-04 10:36:36 +0100

    Moved session data structures to dedicated module

--- warprec/data/entities/__init__.py
+++ warprec/data/entities/__init__.py
@@ -1,8 +1,10 @@
+from . import train_structures
 from .interactions import Interactions
 from .sessions import Sessions
 
 
 __all__ = [
+    "train_structures",
     "Interactions",
     "Sessions",
 ]
---
+++ warprec/data/entities/train_structures/__init__.py
@@ -0,0 +1,3 @@
+from .session_structures import SessionDataset, UserHistoryDataset
+
+__all__ = ["SessionDataset", "UserHistoryDataset"]
