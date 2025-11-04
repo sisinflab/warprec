fed8e205f3dd060914214ea1f5edc8e29787f8db
MarcoWV <marco@wideverse.com>
2025-11-04 11:17:12 +0100

    Circular import

--- warprec/data/__init__.py
+++ warprec/data/__init__.py
@@ -1,18 +1,18 @@
+from .dataset import Dataset, EvaluationDataLoader, NegativeEvaluationDataLoader
 from . import entities
 from . import reader
 from . import splitting
 from . import writer
-from .dataset import Dataset, EvaluationDataLoader, NegativeEvaluationDataLoader
 from .filtering import Filter, apply_filtering
 
 __all__ = [
+    "Dataset",
+    "EvaluationDataLoader",
+    "NegativeEvaluationDataLoader",
     "entities",
     "reader",
     "splitting",
     "writer",
-    "Dataset",
-    "EvaluationDataLoader",
-    "NegativeEvaluationDataLoader",
     "Filter",
     "apply_filtering",
 ]
