d2bf748433af1f03d83bcbdd6bf771d10eb0d098
MarcoWV <marco@wideverse.com>
2025-12-03 12:04:50 +0100

    Added standard optimizer method

--- warprec/common/__init__.py
+++ warprec/common/__init__.py
@@ -1,3 +1,9 @@
 from .initialize import initialize_datasets, prepare_train_loaders, dataset_preparation
+from .optimizers import standard_optimizer
 
-__all__ = ["initialize_datasets", "prepare_train_loaders", "dataset_preparation"]
+__all__ = [
+    "initialize_datasets",
+    "prepare_train_loaders",
+    "dataset_preparation",
+    "standard_optimizer",
+]
