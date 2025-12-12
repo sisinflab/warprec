f51a2dcf80d401dcf3f6ffeda03153c5bd1801ff
MarcoWV <marco@wideverse.com>
2025-12-12 12:49:15 +0100

    xDeepFM model

--- warprec/recommenders/context_aware_recommender/__init__.py
+++ warprec/recommenders/context_aware_recommender/__init__.py
@@ -4,5 +4,6 @@ from .deepfm import DeepFM
 from .fm import FM
 from .nfm import NFM
 from .wideanddeep import WideAndDeep
+from .xdeepfm import xDeepFM
 
-__all__ = ["AFM", "DCN", "DeepFM", "FM", "NFM", "WideAndDeep"]
+__all__ = ["AFM", "DCN", "DeepFM", "FM", "NFM", "WideAndDeep", "xDeepFM"]
