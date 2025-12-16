efc3e9ce24803c2d9c77b7f7ebc14b88dd4be6a3
MarcoWV <marco@wideverse.com>
2025-12-16 14:56:54 +0100

    Added parameter validation for EGCF

--- warprec/utils/config/recommender_model_config/collaborative_filtering_config/graph_based_config.py
+++ warprec/utils/config/recommender_model_config/collaborative_filtering_config/graph_based_config.py
@@ -8,6 +8,7 @@ from warprec.utils.config.model_configuration import (
     FLOAT_FIELD,
     LIST_INT_FIELD,
     BOOL_FIELD,
+    STR_FIELD,
 )
 from warprec.utils.config.common import (
     validate_greater_than_zero,
@@ -16,8 +17,99 @@ from warprec.utils.config.common import (
     validate_layer_list,
     validate_bool_values,
     validate_numeric_values,
+    validate_str_list,
 )
 from warprec.utils.registry import params_registry
+
+
+@params_registry.register("EGCF")
+class EGCF(RecomModel):
+    """Definition of the model EGCF.
+
+    Attributes:
+        embedding_size (INT_FIELD): List of values for embedding_size.
+        n_layers (INT_FIELD): List of values for n_layers.
+        reg_weight (FLOAT_FIELD): List of values for reg_weight.
+        ssl_lambda (FLOAT_FIELD): List of values for ssl_lambda.
+        temperature (FLOAT_FIELD): List of values for temperature.
+        mode (STR_FIELD): List of values for mode.
+        weight_decay (FLOAT_FIELD): List of values for weight_decay.
+        batch_size (INT_FIELD): List of values for batch_size.
+        epochs (INT_FIELD): List of values for epochs.
+        learning_rate (FLOAT_FIELD): List of values for learning rate.
+    """
+
+    embedding_size: INT_FIELD
+    n_layers: INT_FIELD
+    reg_weight: FLOAT_FIELD
+    ssl_lambda: FLOAT_FIELD
+    temperature: FLOAT_FIELD
+    mode: STR_FIELD
+    weight_decay: FLOAT_FIELD
+    batch_size: INT_FIELD
+    epochs: INT_FIELD
+    learning_rate: FLOAT_FIELD
+
+    @field_validator("embedding_size")
+    @classmethod
+    def check_embedding_size(cls, v: list):
+        """Validate embedding_size."""
+        return validate_greater_than_zero(cls, v, "embedding_size")
+
+    @field_validator("n_layers")
+    @classmethod
+    def check_n_layers(cls, v: list):
+        """Validate n_layers."""
+        return validate_greater_than_zero(cls, v, "n_layers")
+
+    @field_validator("reg_weight")
+    @classmethod
+    def check_reg_weight(cls, v: list):
+        """Validate reg_weight."""
+        return validate_greater_equal_than_zero(cls, v, "reg_weight")
+
+    @field_validator("ssl_lambda")
+    @classmethod
+    def check_ssl_lambda(cls, v: list):
+        """Validate ssl_lambda."""
+        return validate_greater_equal_than_zero(cls, v, "ssl_lambda")
+
+    @field_validator("temperature")
+    @classmethod
+    def check_temperature(cls, v: list):
+        """Validate temperature."""
+        return validate_greater_than_zero(cls, v, "temperature")
+
+    @field_validator("mode")
+    @classmethod
+    def check_mode(cls, v: list):
+        """Validate mode."""
+        allowed = ["parallel", "alternating"]
+        return validate_str_list(cls, v, allowed, "mode")
+
+    @field_validator("weight_decay")
+    @classmethod
+    def check_weight_decay(cls, v: list):
+        """Validate weight_decay."""
+        return validate_greater_equal_than_zero(cls, v, "weight_decay")
+
+    @field_validator("batch_size")
+    @classmethod
+    def check_batch_size(cls, v: list):
+        """Validate batch_size."""
+        return validate_greater_than_zero(cls, v, "batch_size")
+
+    @field_validator("epochs")
+    @classmethod
+    def check_epochs(cls, v: list):
+        """Validate epochs."""
+        return validate_greater_than_zero(cls, v, "epochs")
+
+    @field_validator("learning_rate")
+    @classmethod
+    def check_learning_rate(cls, v: list):
+        """Validate learning_rate."""
+        return validate_greater_than_zero(cls, v, "learning_rate")
 
 
 @params_registry.register("GCMC")
