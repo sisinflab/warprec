Diversity metrics user_wise computation fix

warprec/evaluation/metrics/diversity/srecall.py
@@ -142,21 +142,24 @@ class SRecall(TopKMetric):
                 default=torch.zeros(num_users),
                 dist_reduce_fx="sum",
             )  # Initialize a tensor to store metric value for each user
+            self.add_state(
+                "users", default=torch.zeros(num_users), dist_reduce_fx="sum"
+            )
         else:
             self.add_state(
                 "ratio_feature_retrieved",
                 default=torch.tensor(0.0),
                 dist_reduce_fx="sum",
             )  # Initialize a scalar to store global value
-        self.add_state("users", torch.tensor(0.0), dist_reduce_fx="sum")
+            self.add_state("users", torch.tensor(0.0), dist_reduce_fx="sum")
 
         # Add feature lookup as buffer
         self.register_buffer("feature_lookup", feature_lookup)
 
     def update(self, preds: Tensor, user_indices: Tensor, **kwargs: Any):
         """Computes the final value of the metric."""
-        target = kwargs.get("ground", torch.zeros_like(preds))
-        users = kwargs.get("valid_users", self.valid_users(target))
+        target: Tensor = kwargs.get("ground", torch.zeros_like(preds))
+        users: Tensor = kwargs.get("valid_users", self.valid_users(target))
         top_k_indices: Tensor = kwargs.get(
             f"top_{self.k}_indices", self.top_k_values_indices(preds, self.k)[1]
         )
@@ -206,18 +209,24 @@ class SRecall(TopKMetric):
                 unique_feature_counts[non_zero_mask]
                 / unique_relevant_counts[non_zero_mask],
             )
+
+            # Count only users with at least one interaction
+            self.users.index_add_(0, user_indices, users)
         else:
             self.ratio_feature_retrieved += (
                 unique_feature_counts[non_zero_mask]
                 / unique_relevant_counts[non_zero_mask]
             ).sum()
 
-        # Count only users with at least one interaction
-        self.users += users
+            # Count only users with at least one interaction
+            self.users += users.sum()
 
     def compute(self):
         if self.compute_per_user:
             srecall = self.ratio_feature_retrieved
+            srecall[self.users == 0] = float(
+                "nan"
+            )  # Set nan for users with no interactions
         else:
             srecall = (
                 self.ratio_feature_retrieved / self.users