1b1213f8a7a0feec45b4588b8004594ffaed1493
MarcoWV <marco@wideverse.com>
2026-01-20 09:54:22 +0100

    Passing number to Ray Report to avoid errors

--- warprec/recommenders/trainer/objectives.py
+++ warprec/recommenders/trainer/objectives.py
@@ -634,7 +634,7 @@ def validation_report(model: Recommender, **kwargs: Any):
     # If the score has been computed per user we report only the mean
     for key, value in kwargs.items():
         if isinstance(value, Tensor):
-            kwargs[key] = value.nanmean()
+            kwargs[key] = value.nanmean().item()
 
     with tempfile.TemporaryDirectory() as tmpdir:
         torch.save(
