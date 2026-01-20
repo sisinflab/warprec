5cbb63edb581201c18ac774b157457eb46c36c6d
MarcoWV <marco@wideverse.com>
2026-01-20 10:37:30 +0100

    Fix: Correct validation metric computation

--- warprec/recommenders/trainer/objectives.py
+++ warprec/recommenders/trainer/objectives.py
@@ -238,7 +238,11 @@ def objective_function(
                 metric_report["loss"] = epoch_loss / len(train_dataloader)
 
                 # Check for best validation score
-                current_validation_score = metric_report[validation_score]
+                score = metric_report[validation_score]
+                if isinstance(score, Tensor):
+                    current_validation_score = score.nanmean().item()
+                else:
+                    current_validation_score = score
 
                 # Maximize case
                 if mode == "max" and current_validation_score > best_validation_score:
@@ -481,7 +485,11 @@ def objective_function_ddp(config: dict) -> None:
         metric_report["loss"] = epoch_loss / (len(train_dataloader) * world_size)
 
         # Check for best validation score
-        current_validation_score = metric_report[validation_score]
+        score = metric_report[validation_score]
+        if isinstance(score, Tensor):
+            current_validation_score = score.nanmean().item()
+        else:
+            current_validation_score = score
 
         # Maximize case
         if mode == "max" and current_validation_score > best_validation_score:
