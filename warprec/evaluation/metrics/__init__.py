144833e5cc984e989beead519da97a4061b9bb97
MarcoWV <marco@wideverse.com>
2026-02-14 12:10:07 +0100

    Hypervolume metric implementation

--- warprec/evaluation/metrics/__init__.py
+++ warprec/evaluation/metrics/__init__.py
@@ -3,7 +3,17 @@ from . import bias
 from . import coverage
 from . import diversity
 from . import fairness
+from . import multiobjective
 from . import novelty
 from . import rating
 
-__all__ = ["accuracy", "bias", "coverage", "diversity", "fairness", "novelty", "rating"]
+__all__ = [
+    "accuracy",
+    "bias",
+    "coverage",
+    "diversity",
+    "fairness",
+    "multiobjective",
+    "novelty",
+    "rating",
+]
