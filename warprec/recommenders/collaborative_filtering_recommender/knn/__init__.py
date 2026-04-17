3034ad5b6c749a0a7c98c254eba79d50c31090c8
MarcoWV <marco@wideverse.com>
2026-04-17 11:42:41 +0200

    feat: Added UserKNN-TD model implementation

--- warprec/recommenders/collaborative_filtering_recommender/knn/__init__.py
+++ warprec/recommenders/collaborative_filtering_recommender/knn/__init__.py
@@ -1,5 +1,6 @@
 from .itemknn import ItemKNN
 from .itemknntd import ItemKNNTD
 from .userknn import UserKNN
+from .userknntd import UserKNNTD
 
-__all__ = ["ItemKNN", "ItemKNNTD", "UserKNN"]
+__all__ = ["ItemKNN", "ItemKNNTD", "UserKNN", "UserKNNTD"]
--- warprec/utils/config/recommender_model_config/collaborative_filtering_config/knn_config.py
+++ warprec/utils/config/recommender_model_config/collaborative_filtering_config/knn_config.py
@@ -94,3 +94,36 @@ class UserKNN(RecomModel):
     def check_similarity(cls, v: list):
         """Validate similarity."""
         return validate_similarity(cls, v, "similarity")
+
+
+@params_registry.register("UserKNN-TD")
+class UserKNNTD(RecomModel):
+    """Definition of the model UserKNN-TD.
+
+    Attributes:
+        k (INT_FIELD): List of values for neighbor.
+        similarity (STR_FIELD): List of names of similarity functions.
+        beta (FLOAT_FIELD): List of values for decay rate parameter.
+    """
+
+    k: INT_FIELD
+    similarity: STR_FIELD
+    beta: FLOAT_FIELD
+
+    @field_validator("k")
+    @classmethod
+    def check_k(cls, v: list):
+        """Validate k."""
+        return validate_greater_than_zero(cls, v, "k")
+
+    @field_validator("similarity")
+    @classmethod
+    def check_similarity(cls, v: list):
+        """Validate similarity."""
+        return validate_similarity(cls, v, "similarity")
+
+    @field_validator("beta")
+    @classmethod
+    def check_beta(cls, v: list):
+        """Validate beta."""
+        return validate_greater_than_zero(cls, v, "beta")
