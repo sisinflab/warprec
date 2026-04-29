5d2b4d7f4cf50bf489621504e136abb6894c96ba
MarcoWV <marco@wideverse.com>
2026-04-29 10:46:28 +0200

    fix: Train and Swarm pipeline will assign a fallback value to number of GPUs for cuda models with no specified number of cuda devices per trial

--- warprec/pipelines/train.py
+++ warprec/pipelines/train.py
@@ -188,6 +188,11 @@ def train_pipeline(path: str):
         custom_res = params.optimization.custom_resources_per_trial or {}
         label_selector = params.optimization.label_selector or {}
 
+        # Fallback: in case the device is set to cuda but no GPUs are requested,
+        # we set num_gpus to 1 to ensure the correct node is selected
+        if device == "cuda" and num_gpus == 0:
+            num_gpus = 1
+
         # Offload evaluation to worker node
         logger.msg(f"Delegating evaluation of {model_name} model to Ray cluster")
         results, model_evaluation_total_time, inference_time = ray.get(
