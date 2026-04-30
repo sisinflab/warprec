29b583d3916a9e890321bf2a31204a84009b74ec
MarcoWV <marco@wideverse.com>
2026-04-30 11:33:20 +0200

    feat: Added iALS model implementation

--- warprec/recommenders/collaborative_filtering_recommender/latent_factor/__init__.py
+++ warprec/recommenders/collaborative_filtering_recommender/latent_factor/__init__.py
@@ -1,7 +1,8 @@
 from .admmslim import ADMMSlim
 from .bpr import BPR
 from .fism import FISM
+from .ials import iALS
 from .macrmf import MACRMF
 from .slim import Slim
 
-__all__ = ["ADMMSlim", "BPR", "FISM", "MACRMF", "Slim"]
+__all__ = ["ADMMSlim", "BPR", "FISM", "iALS", "MACRMF", "Slim"]
